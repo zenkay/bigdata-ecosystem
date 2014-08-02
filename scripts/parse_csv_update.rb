3f57a2f259555489e8a7aa04efa2353aeba69efa
Andrea Mostosi <andrea.mostosi@zenkay.net>
2014-08-02 16:48:04 +0200

    July 2014 dataset update

--- scripts/parse_csv_update.rb
+++ scripts/parse_csv_update.rb
@@ -1,18 +1,19 @@
-# require "csv"
+require "csv"
+require "json"
 
-# CSV.foreach("./update.csv") do |row|
-#   data = {}
-#   data["name"] = row[0]
-#   data["description"] = row[1]
-#   data["abstract"] = row[1]
-#   data["category"] = row[2]
-#   data["tags"] = []
-#   filename = "#{data["name"].downcase.gsub(/[^a-z0-9]/, "_")}.json"
-#   data["links"] = []
-#   data["links"] << {"text" => "Website", "url" => row[3]} if row[3].present?
-#   data["links"] << {"text" => "Website", "url" => row[4]} if row[4].present?
-#   data["links"] << {"text" => "Website", "url" => row[5]} if row[5].present?
-#   data["links"] << {"text" => "Website", "url" => row[6]} if row[6].present?
-
-#   File.open("./data/#{filename}", 'w') { |file| file.write(JSON.pretty_generate(data)) }
-# end
+CSV.foreach("./update.csv", col_sep: ";", encoding: "iso-8859-1:UTF-8") do |row|
+  puts row[0]
+  data = {}
+  data["name"] = row[0]
+  data["description"] = row[1]
+  data["abstract"] = row[1]
+  data["category"] = row[2]
+  data["tags"] = []
+  filename = "#{data["name"].downcase.gsub(/[^a-z0-9]/, "_")}.json"
+  data["links"] = []
+  data["links"] << {"text" => "Website", "url" => row[3]} unless row[3].nil?
+  data["links"] << {"text" => "Website", "url" => row[4]} unless row[4].nil?
+  data["links"] << {"text" => "Website", "url" => row[5]} unless row[5].nil?
+  data["links"] << {"text" => "Website", "url" => row[6]} unless row[6].nil?
+  File.open("./data/#{filename}", 'w') { |file| file.write(JSON.pretty_generate(data)) }
+end
